ce4527f7b43b2f19c849d14a2fa39377c2583de9
Sumit Gaikwad <sumit.gaikwad@prorigosoftware.com>
2025-10-16 15:41:05 +0530

    Created List

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using DotNetTopics.EnumsExample;
 using DotNetTopics.ParallelProgramming;
 using System;
+using System.Collections.Generic;
 
 namespace DotNetTopics
 {
@@ -10,8 +11,21 @@ namespace DotNetTopics
         {
             
             Console.WriteLine("Hello Form Main");
-            ParallelClass.RunParallelFor();
-            ParallelClass.RunParallelForEach();
+            //ParallelClass.RunParallelFor();
+            //ParallelClass.RunParallelForEach();
+
+            List<EnumOne> student = new List<EnumOne>
+            {
+                new EnumOne{ Name= "Sumit" , Ranks = 1},
+                new EnumOne{ Name = "Vishal" , Ranks = 2},
+                new EnumOne{ Name = "Dnyaneshwar" , Ranks = 3},
+                new EnumOne{ Name = "Aditya" , Ranks = 4}
+            };
+
+            foreach (var item in student)
+            {
+                Console.WriteLine($"{item.Name}'rank = {EnumOne.CheckRank(item.Ranks)}");
+            }
 
             Console.ReadKey();
         }
