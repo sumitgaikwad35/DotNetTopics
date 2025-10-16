665206801bb88c242cf5cc9c00d7f55513721885
Sumit Gaikwad <sumit.gaikwad@prorigosoftware.com>
2025-10-16 11:49:02 +0530

    Add project files.

---
+++ ParallelProgramming/Parallel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DotNetTopics.ParallelProgramming
+{
+    internal class Parallel
+    {
+        public static void Print()
+        {
+            Console.WriteLine("Hello From Parallel");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using DotNetTopics.EnumsExample;
+using DotNetTopics.ParallelProgramming;
+using System;
+
+namespace DotNetTopics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Hello Form Main");
+            Parallel.Print();
+            EnumOne.Print();
+            Console.ReadKey();
+        }
+    }
+}
